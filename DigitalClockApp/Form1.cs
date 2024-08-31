5ec40b4aaa528b5fb4da67a49d7df6fc3b7f5abf
editor2526 <jameson2526@gmail.com>
2024-08-31 11:46:52 +0700

    My first working program
    Feel free to modify it or message me how to improve it.
    Note that this program was created in 2 days and those two days be the start of my coding days.
    Onwards, said the coder who just found a new piece of code :)
    
    
    Signed-off-by: editor2526 <jameson2526@gmail.com>

---
+++ DigitalClockApp/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DigitalClockApp
+{
+    public partial class Form1 : Form
+    {
+        private Timer timer;    
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            timer = new Timer();
+            timer.Interval = 1000; // 1 second
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            Timer_Tick(sender, e, sizeof
+        
+        );
+        }
+
+        private void Timer_Tick(object sender, EventArgs e, int v)
+        {
+            // Get current time
+            int hh = DateTime.Now.Hour;
+            int mm = DateTime.Now.Minute;
+            int ss = DateTime.Now.Second;
+
+            // Format the time string
+            string time = $"{hh:D2}:{mm:D2}:{ss:D2}";
+
+            // Update the label
+            label1.Text = time;
+            v
+        }
+
+
+    }
+}
+        
+    
+
